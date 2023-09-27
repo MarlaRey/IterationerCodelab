f96195b8acf521224db4180b4b1e4e1a3467b93a
MarlaRey <hejmarlenehej@gmail.com>
2023-09-27 09:27:36 +0200

    easy done

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,7 +1,16 @@
 /* opgave 1 */
-console.group("opgave 1");
-// din kode her
 
+// Opret en liste med tal fra 1 til 10 som en tekststreng
+let liste = '';
+for (let i = 1; i < 11; i++) {
+    liste += `<li>${i}</li>`;
+}
+
+// Find det DOM-element, hvor du vil placere listen
+const opgave1Element = document.getElementById('opgave1');
+
+// Indsæt listen som indhold i det valgte element
+opgave1Element.innerHTML = `<ul>${liste}</ul>`;
 
 console.groupEnd();
 
@@ -9,16 +18,33 @@ console.groupEnd();
 /* opgave 2 */
 console.group("opgave 2");
 // din kode her
+let liste2 = '';
+for (let i = 10; i < 12; i++) {
+    liste2 += `<li>${i}</li>`;
 
+  }
+// Find det DOM-element, hvor du vil placere listen
+const opgave1Element2 = document.getElementById('opgave2');
 
+// Indsæt listen som indhold i det valgte element
+opgave1Element2.innerHTML = `<ul>${liste2}</ul>`;
 console.groupEnd();
 
 
 /* opgave 3 */
 console.group("opgave 3");
 // din kode her
+let liste3 = '';
+for (let i = 2; i < 41; i++) {
+    liste3 += `<li>${i}</li>`;
+  }
 
+// Find det DOM-element, hvor du vil placere listen
+const opgave1Element3 = document.getElementById('opgave3');
 
+// Indsæt listen som indhold i det valgte element
+opgave1Element3.innerHTML = `<ul>${liste3}</ul>`;
+console.groupEnd();
 console.groupEnd();
 
 
